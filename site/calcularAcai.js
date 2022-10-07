7d314ba3a14ea9c279434418961ae4e3ce522a83
jaquetoide <pepitoojoness@gmail.com>
2022-10-07 09:40:11 -0300

    Vaiii

---
+++ site/calcularAcai.js
@@ -0,0 +1,25 @@
+function calcularAcai(qtdP, qtdM, qtdG, desc) {
+const a = qtdP = 13.50, qtdM = 15.00, qtdG = 17.50
+let x = qtdP + qtdM + qtdG / desc
+
+return x;
+   
+
+}
+
+return (
+    <main>
+        <div>
+            <h1>Açai pequeno</h1>
+            <button>Quantidade: funcao para aumentar a quantidade (mudar para input) // </button>
+            <h1>Açai médio</h1>
+            <button></button>
+            <h1>Açai grande</h1>
+            <button></button>
+            <h1> Desconto:  </h1>
+            <button> desconto (validar o valor do desconto )</button>
+            * chamar uma funçao calcular o valor do açai*
+            * FUNÇAO QUE MOSTRE O RESULTADO *
+        </div>
+    </main>
+)
