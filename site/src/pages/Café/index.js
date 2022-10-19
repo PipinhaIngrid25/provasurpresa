ae62e29779526c07c1ddb1910b2491a49a0888c4
Ingrid <ra57094432801@acaonsfatima.org.br>
2022-10-19 11:50:50 -0300

    bosta

---
+++ site/src/pages/Café/index.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import './index.scss'
+
+export default function cafe() {
+    const [aluno, setAluno] = useState(0);
+    const [litros, setLitros] = useState(0);
+    const [mililitros, setMililitros] = useState(0);
+    
+    function cafezinho() {
+        
+    }
+}
